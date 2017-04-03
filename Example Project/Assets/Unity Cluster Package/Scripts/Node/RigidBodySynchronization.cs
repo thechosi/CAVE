269f0cfac417d18bc47ac2f585bad38b865ad05d
domin1101 <dominik.winkelbauer@outlook.de>
2017-04-03 23:33:17 +0200

    Improves rigid body synchronization

---
+++ Example Project/Assets/Unity Cluster Package/Scripts/Node/RigidBodySynchronization.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RigidBodySynchronization : MonoBehaviour {
+
+	// Use this for initialization
+    void Start()
+    {
+        if (!GetComponent<NetworkIdentity>().hasAuthority)
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+
+}
