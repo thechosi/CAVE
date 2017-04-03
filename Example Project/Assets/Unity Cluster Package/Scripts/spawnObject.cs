f9529b9f880c1c09d4a911ea17688d76663e9837
Marco Langer <marco.langer@gmail.com>
2017-04-03 14:18:33 +0200

    Add Button
    Add Button to spawn new Objects (with different colors)

---
+++ Example Project/Assets/Unity Cluster Package/Scripts/spawnObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnObject : MonoBehaviour
+{
+
+    public GameObject[] objects;
+    public Vector3 spawnValues;
+    public int objectnr = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void CreateObject()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.y), Random.Range(-spawnValues.x, spawnValues.y), Random.Range(-spawnValues.x, spawnValues.y));
+        objectnr = Random.Range(0, 3); 
+        Instantiate(objects[objectnr], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+    }
+}
