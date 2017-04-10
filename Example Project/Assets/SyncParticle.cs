14350d7c0ce69af2faba95267610c3f2f4ccee89
sea9710 <sea9710@thi.de>
2017-04-10 23:28:48 +0200

    Added SyncParticle script to particle emitter
    related to #8

---
+++ Example Project/Assets/SyncParticle.cs
@@ -0,0 +1,86 @@
+﻿using UnityClusterPackage;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SyncParticle : MonoBehaviour
+{
+    NetworkManager nm;
+    NetworkClient client;
+
+    public void Start()
+    {
+        GameObject go = GameObject.Find("NodeManager");
+        nm = go.GetComponent<NetworkManager>();
+
+        if (NodeInformation.type.Equals("slave"))
+        {
+            client = nm.client;
+            client.RegisterHandler(MyMsgType.ParticleTime, OnParticleTime);
+        }
+    }
+
+    public void Update()
+    {
+        if (NodeInformation.type.Equals("master"))
+        {
+            if (Input.GetKeyDown("o"))
+            {
+                SendParticleInfo(GetSeed(), GetTime());
+                ParticleSystem p = GetParticleSystem();
+            }
+        }
+    }
+
+    private ParticleSystem GetParticleSystem()
+    {
+        return GetComponent<ParticleSystem>();
+    }
+
+    private float GetTime()
+    {
+        ParticleSystem p = GetParticleSystem();
+        //Debug.Log(p.time);
+        return p.time;
+    }
+
+    private uint GetSeed()
+    {
+        ParticleSystem p = GetParticleSystem();
+        //Debug.Log(p.randomSeed);
+        return p.randomSeed;
+    }
+
+    public class MyMsgType
+    {
+        public static short ParticleTime = MsgType.Highest + 1;
+    };
+
+    public class ParticleTimeMessage : MessageBase
+    {
+        public float time;
+        public uint seed;
+    }
+
+    public void SendParticleInfo(uint seed, float time)
+    {
+        ParticleTimeMessage msg = new ParticleTimeMessage();
+        msg.time = time;
+        msg.seed = seed;
+
+        NetworkServer.SendToAll(MyMsgType.ParticleTime, msg);
+    }
+
+    public void OnParticleTime(NetworkMessage netMsg)
+    {
+        ParticleSystem p = GetComponent<ParticleSystem>();
+        ParticleTimeMessage msg = netMsg.ReadMessage<ParticleTimeMessage>();
+        Debug.Log("current playback time " + p.time);
+        Debug.Log("received playback time " + msg.time);
+        //p.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        //p.randomSeed = msg.seed;
+        p.Simulate(msg.time);
+        //p.time = msg.time;
+        //p.Clear();
+        p.Play();
+    }
+}
