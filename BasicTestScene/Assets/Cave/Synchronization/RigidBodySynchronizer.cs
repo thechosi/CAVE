46e15bc43dc33d978568f3e428f7e41a22769b0e
domin1101 <dominik.winkelbauer@outlook.de>
2017-05-20 15:07:57 +0200

    Refactors Cave package

---
+++ BasicTestScene/Assets/Cave/Synchronization/RigidBodySynchronizer.cs
@@ -0,0 +1,52 @@
+﻿using AwesomeSockets.Domain.Sockets;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Cave
+{
+    public class RigidBodySynchronizer : MonoBehaviour
+    {
+
+        public static void Prepare()
+        {
+            Rigidbody[] rigidbodies = Resources.FindObjectsOfTypeAll(typeof(Rigidbody)) as Rigidbody[];
+            foreach (Rigidbody rigidbody in rigidbodies)
+            {
+                if (rigidbody.gameObject.GetComponent<NetworkIdentity>() != null)
+                {
+                    rigidbody.gameObject.AddComponent<NetworkTransform>();
+                }
+            }
+        }
+
+        private static void InitializeNetworkTransforms()
+        {
+            NetworkTransform[] networkTransforms =
+                Resources.FindObjectsOfTypeAll(typeof(NetworkTransform)) as NetworkTransform[];
+            foreach (NetworkTransform networkTransform in networkTransforms)
+            {
+                networkTransform.interpolateMovement = 0;
+                networkTransform.sendInterval = 0.01f;
+                networkTransform.movementTheshold = 0.0001f;
+                networkTransform.transformSyncMode = NetworkTransform.TransformSyncMode.SyncTransform;
+            }
+        }
+
+        public static void InitializeFromServer(Server server, ISocket client)
+        {
+            InitializeNetworkTransforms();
+        }
+
+        public static void InitializeFromClient(Client client)
+        {
+            InitializeNetworkTransforms();
+            Rigidbody[] rigidbodies = Resources.FindObjectsOfTypeAll(typeof(Rigidbody)) as Rigidbody[];
+            foreach (Rigidbody rigidbody in rigidbodies)
+            {
+                Destroy(rigidbody);
+            }
+        }
+
+    }
+
+}
